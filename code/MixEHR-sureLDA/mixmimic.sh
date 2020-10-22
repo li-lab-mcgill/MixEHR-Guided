4ac5b45d13e3494891d83e1b941af907026aad89
yahuja1 <yuri_ahuja@hms.harvard.edu>
2020-10-22 17:50:45 -0400

    Update mixmimic.sh
    Updated for mixEHR-sureLDA (as opposed to mixEHR)

--- code/MixEHR-sureLDA/mixmimic.sh
+++ code/MixEHR-sureLDA/mixmimic.sh
@@ -2,17 +2,18 @@
 
 ehrdata=./mixmimic/mimic_trainData.txt
 ehrmeta=./mixmimic/mimic_meta.txt
+ehrprior=./mixmimic/MIMIC_prior_phecodes1d.txt
 
-K=75
+K=1515
 # K=50
 
-niter=500
+niter=100
 
 # mar model (evaluation purpose only)
 # ./mixehr --outputIntermediates -f $ehrdata -m $ehrmeta -k $K -i $niter --inferenceMethod JCVB0 --mar
 
 # nmar model
-./mixehr --outputIntermediates -f $ehrdata -m $ehrmeta -k $K -i $niter --inferenceMethod JCVB0 
+./mixehr --outputIntermediates -f $ehrdata -m $ehrmeta -trp $ehrprior -k $K -i $niter --inferenceMethod JCVB0 --maxcores 10
 
 # stochastic variational inference for massive scale EHR
 # bathsize=5000
